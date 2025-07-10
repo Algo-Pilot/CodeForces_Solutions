6186b0fe0f75845fc0db4c5df671803b05adfd4c
Algo-Pilot <fahimasfaq01@gmail.com>
2025-07-10 23:33:32 +0600

    new

---
+++ Java/1030A - In Search of an Easy Problem.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int count = 0;
+        for(int i = 0; i < num; i++){
+            int a = sc.nextInt();
+            if(a == 1){
+                count++;
+                break;
+            }
+        }
+        if (count > 0){
+            System.out.println("HARD");
+        }
+        else{
+            System.out.println("EASY");
+        }
+    }
+}
