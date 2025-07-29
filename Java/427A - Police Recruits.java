bb74ba0c111fe2c0d0d330adf67fa98e3d8c1dad
FAT CodeSmith <fahimasfaq01@gmail.com>
2025-07-29 14:49:05 +0600

    Create 427A - Police Recruits.java

---
+++ Java/427A - Police Recruits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int officer = 0;
+        int count = 0;
+        for(int i = 0;i < num; i++){
+            int x = sc.nextInt();
+            if(x == -1){
+                if(officer > 0){
+                    officer--;
+                }
+                else{
+                    count++;
+                }
+            }
+            else{
+                officer += x;
+            }
+        }
+        System.out.println(count);
+    }
+}
