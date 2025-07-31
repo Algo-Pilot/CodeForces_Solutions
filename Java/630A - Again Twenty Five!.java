8e912cd0a4eb79ff1cfc29486be8fade35405879
FAT CodeSmith <fahimasfaq01@gmail.com>
2025-08-01 00:28:04 +0600

    Create 630A - Again Twenty Five!.java

---
+++ Java/630A - Again Twenty Five!.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int val = (int) Math.pow(5, n);
+        int  x = val % 100;
+        System.out.println(x);
+    }
+}
+write this code exactly in c
