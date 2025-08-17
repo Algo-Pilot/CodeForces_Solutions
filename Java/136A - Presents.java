9c59424f60f62c0c4206f50d2210d481864b39d4
FAT CodeSmith <fahimasfaq01@gmail.com>
2025-08-17 22:25:26 +0600

    Update 136A - Presents.java

--- Java/136A - Presents.java
+++ Java/136A - Presents.java
@@ -1,13 +1,10 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
- 
-/* Name of the class has to be "Main" only if the class is public. */
 public class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes herezzz
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int a[]=new int[n];
